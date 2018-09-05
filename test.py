bd390a7987819a88a3b3926ca8ef22bd15267a23
echalmers <dchalmer@ualberta.ca>
2018-09-05 15:37:25 -0600

    .

--- test.py
+++ test.py
@@ -8,29 +8,34 @@ r = requests.get('http://www.orcpub.com/dungeons-and-dragons/5th-edition/monster
 html = r.content.decode()
 
 # get all monster links
-links = set(re.findall('/dungeons-and-dragons/5th-edition/monsters/.+?"', html))
-links = [x[:-1] for x in links]
+# links = set(re.findall('/dungeons-and-dragons/5th-edition/monsters/.+?"', html))
+# links = [x[:-1] for x in links]
+links = ['/dungeons-and-dragons/5th-edition/monsters/ancient-brass-dragon']
 
 # process each monster page
 key_re = re.compile(':\S+? ')
 comma_re = re.compile('}\s+\{')
-for link in ['/dungeons-and-dragons/5th-edition/monsters/couatl']: #links[:40]:
-    print(link)
+for i in range(0, len(links)):
+    link = links[i]
+    print(link + ' ({num}/{total})'.format(num=i+1, total=len(links)))
     r = requests.get('http://www.orcpub.com' + link)
     html = r.content.decode()
 
     # extract the json-ish part
     html = html.split('id="embedded-data">')[1]
     html = html.split('</div>')[0]
-    print(html)
+
+    # replace newlines
+    html = html.replace('\\n', '\n')
 
     # convert keys
+    num = 0
     while True:
         match = key_re.search(html)
         if match is None:
             break
+
         html = html.replace(match.group(), '"' + match.group()[1:-1] + '": ')
-        print(html)
 
     # add missing commas
     html = comma_re.sub('}, {', html)
@@ -41,7 +46,7 @@ for link in ['/dungeons-and-dragons/5th-edition/monsters/couatl']: #links[:40]:
         .replace('"challenge": 1/2', '"challenge": 0.5')
 
     try:
-        print(json.loads(html))
+        print(json.loads(html, strict=False))
     except Exception as ex:
         print(html)
         raise ex
