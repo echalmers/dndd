65de164d8c191bb5211fff4c07f6b8461fafa13c
Eric <eric.chalmers@uleth.ca>
2018-09-18 18:50:12 -0600

    adding combat models

--- combat/models.py
+++ combat/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from monsters.models import Monster
+from players.models import Player
 
-# Create your models here.
+class PcCombatant(models.Model):
+    display_name = models.CharField()
+    player = models.ForeignKey(Player, on_delete=None)
+
+class NpcCombatant(models.Model):
+    display_name = models.CharField()
+    monster = models.ForeignKey(Monster, on_delete=None)
+
+
+
